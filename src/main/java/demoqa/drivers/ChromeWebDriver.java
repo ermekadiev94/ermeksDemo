4ed8e65c3ccb5db65d6ceacec5c9fca1c3957b0c
Nursultan <musakunovnur@gmail.com>
2024-10-11 10:47:20 +0600

    123

--- src/main/java/demoqa/drivers/ChromeWebDriver.java
+++ src/main/java/demoqa/drivers/ChromeWebDriver.java
@@ -1,33 +1,29 @@
 package demoqa.drivers;
 
-import demoqa.utils.ConfigReader;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
+import static demoqa.utils.ConfigReader.getValue;
+
 public class ChromeWebDriver {
 
     public static WebDriver loadChromeDriver() {
-        WebDriverManager.chromedriver().setup();
-
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--disable-extensions");
-        options.addArguments("--window-size=1920,1080");
-        options.addArguments("--no-sandbox");
-        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-
-        if (Boolean.parseBoolean(ConfigReader.getValue("headless"))) {
-            options.addArguments("--headless");
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--remote-allow-origins=*");
+            options.addArguments("--disable-extensions");
+            options.addArguments("--window-size-1920,1080");
+            options.addArguments("--no-sandbox");
+            if(Boolean.parseBoolean(getValue("headless"))) {
+                options.addArguments("--headless");
+            }
+            WebDriver driver = new ChromeDriver(options);
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+            driver.manage().window().maximize();
+            return driver;
         }
-
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-        return driver;
-    }
 }
