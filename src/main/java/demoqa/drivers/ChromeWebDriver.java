c10e6e9c7e5744545bacee6f11ffc385aa8cb206
Nursultan <musakunovnur@gmail.com>
2024-10-24 11:12:06 +0600

    remote webdriver

--- src/main/java/demoqa/drivers/ChromeWebDriver.java
+++ src/main/java/demoqa/drivers/ChromeWebDriver.java
@@ -4,7 +4,10 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 import static demoqa.utils.ConfigReader.getValue;
@@ -12,18 +15,24 @@ import static demoqa.utils.ConfigReader.getValue;
 public class ChromeWebDriver {
 
     public static WebDriver loadChromeDriver() {
-            WebDriverManager.chromedriver().setup();
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--remote-allow-origins=*");
-            options.addArguments("--disable-extensions");
-            options.addArguments("--window-size-1920,1080");
-            options.addArguments("--no-sandbox");
-            if(Boolean.parseBoolean(getValue("headless"))) {
-                options.addArguments("--headless");
-            }
-            WebDriver driver = new ChromeDriver(options);
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-            driver.manage().window().maximize();
-            return driver;
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--no-sandbox");
+        if (Boolean.parseBoolean(getValue("headless"))) {
+            options.addArguments("--headless");
         }
+        // Specify the URL of the remote WebDriver server
+        WebDriver driver = null;
+        try {
+            driver = new RemoteWebDriver(new URL("http://selenium-hub:4444/wd/hub"), options);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().window().maximize();
+        return driver;
+    }
 }
